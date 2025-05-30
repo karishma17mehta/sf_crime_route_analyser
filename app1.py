4eded4f324819320e0f1670616f7d06b3e198e02
Karishma Mehta <karishma17mehta@gmail.com>
2025-05-29 23:41:50 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,10 +8,6 @@ import numpy as np
 import openrouteservice
 import folium
 from streamlit_folium import st_folium
-from utils import (
-    create_ors_client, geocode_address, get_route_coords, assess_route,
-    iterative_reroute_min_risk, plot_route_on_map
-)
 
 # --- Load environment variables ---
 load_dotenv("py.env")
