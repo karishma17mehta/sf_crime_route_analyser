e8735ebeca9a1dfa9ee72ee4afac61a9782e50c9
Karishma Mehta <karishma17mehta@gmail.com>
2025-05-29 23:30:36 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,7 +8,7 @@ import numpy as np
 import openrouteservice
 import folium
 from streamlit_folium import st_folium
-from utils import (
+from .utils import (
     create_ors_client, geocode_address, get_route_coords, assess_route,
     iterative_reroute_min_risk, plot_route_on_map
 )
